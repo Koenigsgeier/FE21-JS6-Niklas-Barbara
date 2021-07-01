f5bc3cb4ca26476a6bbbd7c1ceec140a4ce6d001
BarbaraGaluppo <barbaragaluppo@gmail.com>
2021-07-01 14:53:39 +0200

    fixes typos inter

---
+++ BARBARA/ADVANCED/advanced.js
@@ -0,0 +1,8 @@
+// Create an external JSON file that will contain the data( e.g. 
+//     `{'title': 'Javascript for Dummies',
+//      'author': 'Dummy Dumb Dumb',
+//      'read': 'false'
+//     }`
+//   Iterate through the array of books. For each book, create a <p> element with the book title and author and append it to the page.
+//   Each book should have an image cover.
+//   Change the style of the book depending on whether you have read it or not.
