5ae46d604514718749d5b6a25909caff452bda39
BarbaraGaluppo <barbaragaluppo@gmail.com>
2021-07-01 11:00:14 +0200

    adds basic1

---
+++ BARBARA/basic.js
@@ -0,0 +1,36 @@
+
+// My favorite sandwich is a Hamburger which has approximately 260 calories, along with it I enjoy eating Large French Fries which have about 570 calories.
+
+// var sandwiches = `{ 
+//     "sandwich": "hamburger", 
+//     "calories": "260" 
+// }`;
+
+// var fries = `{ 
+//     "fries_size": "Large French Fries", 
+//     "calories": "570" 
+// }`;
+
+
+
+
+
+// let object1  = { 
+//     "sandwich": "hamburger", 
+//     "calories": 260
+// };
+
+// let object2= { 
+//     "fries_size": "Large French Fries", 
+//     "calories": 570
+// };
+
+// document.getElementById('result').innerText = " My favorite sandwich is a " + object1.sandwich + " which has approximately " + object1.calories + " calories " + " , along with it I enjoy eating " + object2.fries_size + " which have about " + object2.calories + " calories ";
+
+
+
+
+// Print object1
+
+// document.getElementById('result').innerText = " My favorite sandwich is a " + var sandwiches.sandwich + " which has approximately " + var sandwiches.calories + " calories " + " , along with it I enjoy eating " + var Fries.fries_size + " which have about " + var Fries.calories + " calories ";
+
