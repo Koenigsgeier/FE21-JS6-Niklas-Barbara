c2f38c0f57d1214f10ea8aa8461daa541432aef0
BarbaraGaluppo <barbaragaluppo@gmail.com>
2021-07-01 14:16:36 +0200

    Adds intermediate

---
+++ BARBARA/INTER/inter.js
@@ -0,0 +1,25 @@
+
+
+// function CreateTable(){
+//     let = employees = [
+//         {
+//             "uniqueId": "123098",
+//             "firstName": "Otto",
+//             "lastName": "Smith",
+//             "emailAddress": "o.smith@gmail.com",
+//             "jobTitle": "Executive Assistant",
+//             "salary": "3000 euros"
+//         },
+//         {
+
+//         "uniqueId": "465728",
+//         "firstName": "Anna",
+//         "lastName": "Smith",
+//         "emailAddress": "a.smith@gmail.com",
+//         "jobTitle": "Marketing Manager",
+//         "salary": "4000 euros"
+    
+//         }
+
+
+
